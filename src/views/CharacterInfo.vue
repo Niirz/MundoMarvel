832f1f069ce9d5d0e82407dd5bd11dfc1b0a0e4e
ricardoCs7 <r.carrasco05@ufromail.cl>
2021-12-20 18:50:09 -0300

    actualizadas las vistas y scss

--- src/views/CharacterInfo.vue
+++ src/views/CharacterInfo.vue
@@ -1,24 +1,42 @@
-<template lang="">
-<!-- {{character}}-->
-{{character.thumbnail}}
-    <div class="row">
-      <h1>{{ character.name }}</h1>
-      <div class="col-sm-4">
-          {{ character.name }}
-          insertar foto
-      </div>
-      <div v-if="character.description!=''" class="col-md-6">
-        <span
-          ><b><h3>Description:</h3></b></span>
-          {{ character.description }}
-      </div>
-      <div v-else class="col-md-6">
-        <span
-          ><b><h3>Description:</h3></b></span>
-          No tiene descripcion
+<template >
+  <!-- {{character}}-->
+
+  <div id="characterInfo" class="row">
+    <h1>{{ character.name }}</h1>
+    <div>
+      <hr />
+    </div>
+    <div class="col-sm-4">
+      <img :src="character.thumbnail + '.jpg'" alt="" width="250" />
+    </div>
+    <br />
+    <br />
+    <div v-if="character.description != ''" class="col-md-6">
+      <span
+        ><b><h3>Description:</h3></b></span
+      >
+      <p>{{ character.description }}</p>
+    </div>
+    <div v-else class="col-md-6">
+      <b><h3 style="text-align: left">Description:</h3></b>
+      <div class="row">
+        <div class="col">
+          <p>No se ha encontrado descripción :(</p>
+          <br />
+          Pero no te preocupes, solo algunas no la poseen! :D
+        </div>
+        <div class="col">
+          <span>
+            <img
+              src="https://www.clipartmax.com/png/full/50-508951_cartoon-art-pictures-sad-deadpool.png"
+              width="250"
+              alt=""
+          /></span>
+        </div>
       </div>
     </div>
-    <section>
+  </div>
+  <section>
     <button @click="$router.go(-1)" type="button" class="btn btn-danger">
       <i class="bi bi-arrow-left-circle"></i> Regresar
     </button>
@@ -27,7 +45,7 @@
 <script>
 import marvelApi from "@/services/marvelApi.js";
 export default {
-    data() {
+  data() {
     return {
       character: [],
       id: {},
@@ -41,12 +59,11 @@ export default {
       fetch(marvelApi.getCharacter(id))
         .then((res) => res.json())
         .then((json) => {
-            this.character=json.data.results[0];
+          this.character = json.data.results[0];
         });
     },
   },
-}
+};
 </script>
-<style lang="">
-    
+<style >
 </style>
--- src/views/ComicInfo.vue
+++ src/views/ComicInfo.vue
@@ -1,25 +1,42 @@
 <template>
   <section v-for="comic in comics" :comic="comic">
-    <div class="row">
+    <div id="comicInfo" class="row">
       <h1>{{ comic.title }}</h1>
+      <div>
+        <hr />
+      </div>
       <div class="col-sm-4">
         <img :src="comic.thumbnail.path + '.jpg'" alt="" width="250" />
       </div>
-      <div v-if="comic.description!=''" class="col-md-6">
+      <div v-if="comic.description != ''" class="col-md-6">
         <span
-          ><b><h3>Description:</h3></b></span>
-          {{ comic.description }}
+          ><b><h3>Description:</h3></b></span
+        >
+       <p> {{ comic.description }}</p>
       </div>
       <div v-else class="col-md-6">
-        <span
-          ><b><h3>Description:</h3></b></span>
-          No tiene descripcion
+        <b><h3 style="text-align: left">Description:</h3></b>
+        <div class="row">
+          <div class="col">
+            <p>No se ha encontrado descripción :(</p>
+            <br />
+            Pero no te preocupes, solo algunas no la poseen! :D
+          </div>
+          <div class="col">
+            <span>
+              <img
+                src="https://www.clipartmax.com/png/full/50-508951_cartoon-art-pictures-sad-deadpool.png"
+                width="250"
+                alt=""
+            /></span>
+          </div>
+        </div>
       </div>
       <div v-for="date in dates" :date="date">
-        <span>Dates: </span> {{comic.date}}
+        <span>Dates: </span> {{ comic.date }}
       </div>
     </div>
-    
+
     <!-- <div v-for="image in images" :image="image">
       <img :src="comic.images" alt="" />
     </div> -->
@@ -33,6 +50,7 @@
 </template>
 <script>
 import marvelApi from "@/services/marvelApi.js";
+
 export default {
   data() {
     return {
