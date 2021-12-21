9c29894c31f3707685d6b6a0b0bb587eab90db8b
ricardoCs7 <r.carrasco05@ufromail.cl>
2021-12-21 18:29:17 -0300

    cambiada apikey

--- src/services/marvelApi.js
+++ src/services/marvelApi.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-//1f37f182e835b539fa14f4bd4d92138237cf29af63e39aee8077ad9d2bd967a7fa47f4f1b
-//hash: 1449909229a1a077fc7e4286d43b2194
-//ts=1&apikey=3e39aee8077ad9d2bd967a7fa47f4f1b&hash=1449909229A1A077FC7E4286D43B2194
-//const baseUrl = 'https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=3e39aee8077ad9d2bd967a7fa47f4f1b&hash=1449909229a1a077fc7e4286d43b2194';
-//apikey = ?ts=1&apikey=3e39aee8077ad9d2bd967a7fa47f4f1b&hash=1449909229a1a077fc7e4286d43b2194 
+//10e6e3167a7ea12cf17152fa8d3f27a3b680f3f0a13214488292737a463bec9bc315b8826
+//hash: 013380175212d9b51d0942e070802e5e
+//ts=1&apikey=13214488292737a463bec9bc315b8826&hash=013380175212d9b51d0942e070802e5e
+//const baseUrl = 'https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=13214488292737a463bec9bc315b8826&hash=013380175212d9b51d0942e070802e5e';
+//apikey = ?ts=1&apikey=13214488292737a463bec9bc315b8826&hash=013380175212d9b51d0942e070802e5e
 const baseUrl = 'https://gateway.marvel.com:443/v1/public/'
-        //const apikeyPriv = `f37f182e835b539fa14f4bd4d92138237cf29af6`;
-        //const apikeyPublic = `3e39aee8077ad9d2bd967a7fa47f4f1b`;
+        //const apikeyPriv = `0e6e3167a7ea12cf17152fa8d3f27a3b680f3f0a`;
+        //const apikeyPublic = `13214488292737a463bec9bc315b8826`;
         
 class marvelApi {
     constructor(){
@@ -17,25 +17,25 @@ class marvelApi {
         
     }
     getComics(limit){
-        //const apiKey = `ts=1&apikey=3e39aee8077ad9d2bd967a7fa47f4f1b&hash=1449909229a1a077fc7e4286d43b2194`;
+        //const apiKey = `ts=1&apikey=13214488292737a463bec9bc315b8826&hash=013380175212d9b51d0942e070802e5e`;
         
-        const urlbas = 'https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=3e39aee8077ad9d2bd967a7fa47f4f1b&hash=1449909229a1a077fc7e4286d43b2194'
+        const urlbas = 'https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=13214488292737a463bec9bc315b8826&hash=013380175212d9b51d0942e070802e5e'
         //return urlbas;
-        return `https://gateway.marvel.com:443/v1/public/comics?limit=${limit}&ts=1&apikey=3e39aee8077ad9d2bd967a7fa47f4f1b&hash=1449909229a1a077fc7e4286d43b2194`
+        return `https://gateway.marvel.com:443/v1/public/comics?limit=${limit}&ts=1&apikey=13214488292737a463bec9bc315b8826&hash=013380175212d9b51d0942e070802e5e`
         //return this.resource.get(`${baseUrl}comics?${apiKey}`);
     }
     getComic(id){
-        //const apiKey = `ts=1&apikey=3e39aee8077ad9d2bd967a7fa47f4f1b&hash=1449909229a1a077fc7e4286d43b2194`;
+        //const apiKey = `ts=1&apikey=13214488292737a463bec9bc315b8826&hash=013380175212d9b51d0942e070802e5e`;
         //return this.resource.get(`${baseUrl}comics/${id}?${apiKey}`);
-        //const urlbas = 'https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=3e39aee8077ad9d2bd967a7fa47f4f1b&hash=1449909229a1a077fc7e4286d43b2194'
-        return `https://gateway.marvel.com:443/v1/public/comics/${id}?ts=1&apikey=3e39aee8077ad9d2bd967a7fa47f4f1b&hash=1449909229a1a077fc7e4286d43b2194`
+        //const urlbas = 'https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=13214488292737a463bec9bc315b8826&hash=013380175212d9b51d0942e070802e5e'
+        return `https://gateway.marvel.com:443/v1/public/comics/${id}?ts=1&apikey=13214488292737a463bec9bc315b8826&hash=013380175212d9b51d0942e070802e5e`
     }
     getCharacters(limit){
-        return `https://gateway.marvel.com:443/v1/public/characters?limit=${limit}&ts=1&apikey=3e39aee8077ad9d2bd967a7fa47f4f1b&hash=1449909229a1a077fc7e4286d43b2194`
+        return `https://gateway.marvel.com:443/v1/public/characters?limit=${limit}&ts=1&apikey=13214488292737a463bec9bc315b8826&hash=013380175212d9b51d0942e070802e5e`
     }
 
     getCharacter(id){
-        return `https://gateway.marvel.com:443/v1/public/characters/${id}?ts=1&apikey=3e39aee8077ad9d2bd967a7fa47f4f1b&hash=1449909229a1a077fc7e4286d43b2194`
+        return `https://gateway.marvel.com:443/v1/public/characters/${id}?ts=1&apikey=13214488292737a463bec9bc315b8826&hash=013380175212d9b51d0942e070802e5e`
     }
 };
 export default new marvelApi();
