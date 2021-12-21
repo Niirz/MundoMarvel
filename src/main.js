dc592815e69ecd729baf26d6f0c86ee287349092
ricardoCs7 <r.carrasco05@ufromail.cl>
2021-12-20 21:56:50 -0300

    import swal

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from '@/router'
 import "./assets/scss/index.scss";
+import swal from 'sweetalert';
+import Swal from 'sweetalert2'
 
 const app = createApp(App);
 app.use(router);
